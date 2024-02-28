a1c3fbdc3938611ae5a6c9e883a1af250cf1fc3c
RashmiShinde21 <wwwrashmishinde2002@gmail.com>
2024-02-28 17:09:39 +0530

    sprint2 version2

--- main.cpp
+++ main.cpp
@@ -10,11 +10,21 @@ int main() {
   HomeOwner h1;
   EnvSensor E;
   SmartGeyser S;
+  int var1;
 
 
   cout<<h1.getownerinfo();
   E.settemp();
-  S.geyseron();
+  cout<<"Enter 1 to on the geyser and  0 for geyser off"<<endl;
+  cin>>var1;
+
+  if(var1==1)
+  {S.geyseron();}
+  if(var1==0)
+  {
+
+      S.geyseroff();
+  }
 
 
   return 0;
